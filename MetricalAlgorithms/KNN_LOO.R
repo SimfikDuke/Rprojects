10c66a8f29fdfd6a2dd6f5eb28d078b8024effed
SimfikDuke <duke@simfik.ru>
2018-10-09 21:12:09 +0300

    0910

--- MetricalAlgorithms/KNN_LOO.R
+++ MetricalAlgorithms/KNN_LOO.R
@@ -94,9 +94,7 @@ classMapKWNN <- function(ir=iris,k=6,w=0.5){
     }
   }
 }
-
-#Функция отрисовка графика оценки точности алгоритма KNN при помощи скользящего контроля
-LOOKNN <- function(x){
+badLOOKNN <- function(x){
   len <- dim(x)[1]
   n <- dim(x)[2]-1
   mark <- rep(0,len)
@@ -115,12 +113,64 @@ LOOKNN <- function(x){
   }
   plot(mark,type = "l",main = "LOO(k) алгоритма KNN",xlab = "k", ylab="оценка")
 }
-
+#Функция отрисовка графика оценки точности алгоритма KNN при помощи скользящего контроля
+LOOKNN <- function(x){
+  len <- dim(x)[1]
+  n <- dim(x)[2]-1
+  mark <- rep(0,len)
+  for(i in 1:len){
+    x1 <- x[-i,]
+    x1_m <- dim(x1)[1]
+    dist <- matrix(NA, x1_m, n)
+    for (j in x1_m:1){
+      dist[j,] <- c(j, eDist(x1[j,1:n], x[i,1:n]))
+    }  
+    ordered <- order(dist[,2])
+    for(k in 1:len){
+      classes <- x[ordered[1:k],n+1]
+      class <- names(which.max(table(classes)))
+      if(class != x[i,n+1]) {
+        mark[k] <- mark[k] + 1/len
+      }
+    }
+  }
+  print(mark)
+  print(which.min(mark))
+  plot(mark,type = "l",main = "LOO(k) алгоритма KNN",xlab = "k", ylab="оценка")
+}
+newLOOKWNN <- function(x, k=6){
+  m <- dim(x)[1]
+  n <- dim(x)[2]-1
+  q <- seq(0.05,0.95,0.05)
+  len <- length(q)
+  mark <- rep(0,len)
+  for(i in 1:m){
+    x1 <- x[-i,]
+    x1_m <- dim(x1)[1]
+    dist <- matrix(NA, x1_m, n)
+    for (j in x1_m:1){
+      dist[j,] <- c(j, eDist(x1[j,1:n], x[i,1:n]))
+    }  
+    ordered <- x1[order(dist[,2])[1:k],]
+    classes <- names(table(ordered[,n+1]))
+    for(w in 1:len){
+      rang <- rep(0,length(classes))
+      for(j in 1:k) rang[ordered[j,n+1]] <- rang[ordered[j,n+1]] + w^q[j]
+      class <- classes[which.max(rang)]
+      if(class != x[i,n+1]) {
+        mark[w] <- mark[w] + 1/m
+      }
+    }
+  }
+  print(mark)
+  print(which.min(mark))
+  plot(q,mark,type = "l",main = "LOO(k,w) алгоритма KWNN",xlab = "k", ylab="оценка")
+}
 #Функция отрисовка графика оценки точности алгоритма KWNN при помощи скользящего контроля
 LOOKWNN <- function(x,k=6){
   n <- dim(x)[2]-1
   m <- dim(x)[1]
-  q <- seq(0.05,0.95,0.05)
+  q <- seq(0.05,1,0.05)
   len <- length(q)
   mark <- matrix(0,len,2)
   for(i in 1:len){
@@ -159,15 +209,20 @@ marginKWNN <- function(x,k=6,w=0.5){
   return(margin)
 }
 
-read.table("my__")
-plotIris(listIrises, "Карта классификации 1NN")
-classMapFNN(listIrises)
-plotIris(listIrises, "Карта классификации KNN, k=6")
-classMapKNN(listIrises)
-plotIris(listIrises, "Карта классификации KWNN, k=6, w=0.5")
-classMapKWNN(listIrises)
-LOOKNN(listIrises)
-LOOKWNN(listIrises)
+listIrises <- read.table("my__")
+#plotIris(listIrises, "Карта классификации 1NN")
+#classMapFNN(listIrises)
+#plotIris(listIrises, "Карта классификации KNN, k=6")
+#classMapKNN(listIrises)
+#plotIris(listIrises, "Карта классификации KWNN, k=6, w=0.5")
+#classMapKWNN(listIrises)
+#LOOKNN(iris[,3:5])
+k <- 19
+w <- 0.25
+classMapKWNN(iris,k,w)
+newLOOKWNN(listIrises[,3:5],k)
+newLOOKWNN(iris[,3:5],k)
+#LOOKWNN(listIrises)
   #plotKWNN(2.5,1,listIrises,6,0.5)
   #classMapKNN(listIrises)
   #LOOKWNN(listIrises)
